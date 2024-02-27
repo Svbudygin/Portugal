f8c19c38eb79502a9c1431eeebb975de042b9cac
sergeybudygin <svbudygin@yandex.ru>
2024-02-27 13:38:53 +0300

    pars

--- parsing/imovirtual/main.py
+++ parsing/imovirtual/main.py
@@ -1,22 +1,26 @@
+import json
+
 from bs4 import BeautifulSoup
 import requests
 
 from config import cookies, headers
 
 
-def pars_exect_flat(*args):
-    # URL = 'https://www.imovirtual.com/pt/anuncio/apartamento-t0-em-paranhos-ID1e0c3.html#71766add10'
-    URL = 'https://www.imovirtual.com/pt/anuncio/apartamento-t1-perto-da-praia-com-vista-mar-e-garagem-em-quarteira-ID1bcgB.html#ab04badaa0'
+def pars_exact_flat(url: str) -> dict:
     response = requests.get(
-        URL,
+        url,
         cookies=cookies,
         headers=headers,
     )
 
     html = BeautifulSoup(response.text, 'lxml')
+    with open('bs.json', 'w') as f:
+        json.dump(str(html), f)
+    image_tag = html.find('picture').find('img')
     name = html.find('header', class_='css-sos826 efcnut31').find('h1', class_="css-1wnihf5 efcnut38").text
     main_data = html.find('div', class_="css-c078ty e17tijfc0")
     data = main_data.find_all('div', class_="css-1v52owc e1qm3vsd0")
+    photo_link = image_tag.get('src') if image_tag else None
     price = html.find('header', class_='css-sos826 efcnut31').find('strong').text
     data_for_bot = {}
     for e in data:
@@ -25,11 +29,13 @@ def pars_exect_flat(*args):
         data_for_bot[title] = text
     data_for_bot['price'] = price
     data_for_bot['name'] = name
-    print(data_for_bot)
+    data_for_bot['photo'] = photo_link
+    return data_for_bot
 
 
-def pars(*args):
-    URL = 'https://www.imovirtual.com/comprar/apartamento/'
+def pars(URL):
+    # URL = 'https://www.imovirtual.com/arrendar/'
+
     response = requests.get(
         URL,
         cookies=cookies,
@@ -37,13 +43,15 @@ def pars(*args):
     )
 
     html = BeautifulSoup(response.text, 'lxml')
-    name = list(map(lambda x: x.get('data-url'),
-                    html.find('div', class_="row section-listing__row").find('div',
-                                                                             class_="col-md-content section-listing__row-content").find_all(
-                        'article')))
-    print(name)
+    list_with_all = list(map(lambda x: x.get('data-url'),
+                             html.find('div',
+                                       class_="row section-listing__row").find('div',
+                                                                               class_="col-md-content section-listing__row-content").find_all(
+                                 'article')))
+    return list_with_all
 
 
 if __name__ == "__main__":
     pass
-pars()
+# pars_exact_flat('https://www.imovirtual.com/pt/anuncio/arrendamento-de-loja-escritorios-em-setubal-ID1dUzP.html#5381dae35a')
+pars_exact_flat('https://www.imovirtual.com/pt/anuncio/arrendamento-espaco-comercial-ID1e0q0.html#5381dae35a')
