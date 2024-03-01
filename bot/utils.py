b06af4f8c4a3ced916ffcbfc9581f2fa9e860ce3
sergeybudygin <svbudygin@yandex.ru>
2024-03-01 09:37:48 +0300

    pars

--- bot/utils.py
+++ bot/utils.py
@@ -1,3 +1,6 @@
+import json
+
+import requests
 from aiogram.utils.exceptions import MessageToDeleteNotFound
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -38,11 +41,22 @@ class Form(StatesGroup):
     from_date_state = State()
     to_date_state = State()
 
-def get_link(topologia:list, price_from, price_to):
-    price = f'?search%5Bfilter_float_price%3Afrom%5D={price_from}&search%5Bfilter_float_price%3Ato%5D={price_to}'
-    f'?search%5Bfilter_float_price%3Ato%5D={price_to}'
-    f'?search%5Bfilter_float_price%3Afrom%5D={price_from}'
-    topologia = f'?'+'&search%5Bfilter_enum_rooms_num%5D%5B2%5D='.join(topologia)[1:]
-    '?search%5Border%5D=created_at_first%3Adesc' # сортировка по дате анонса
+def get_link(city, area, topologia:list, price_from, price_to):
+    price = f'search%5Bfilter_float_price%3Afrom%5D={price_from}&search%5Bfilter_float_price%3Ato%5D={price_to}'
+    f'search%5Bfilter_float_price%3Ato%5D={price_to}'
+    f'search%5Bfilter_float_price%3Afrom%5D={price_from}'
+    topologia = '&search%5Bfilter_enum_rooms_num%5D%5B2%5D='.join(topologia)[1:]
+    'search%5Border%5D=created_at_first%3Adesc' # сортировка по дате анонса
+    # url = f'https://www.imovirtual.com/ajax/geo6/autosuggest/?data={city}%20{area}&lowPriorityStreetsSearch=true&levels%5B0%5D=REGION&levels%5B1%5D=SUBREGION&levels%5B2%5D=CITY&levels%5B3%5D=DISTRICT&levels%5B4%5'
+    url = 'https://www.imovirtual.com/ajax/geo6/autosuggest/?data=lisabon%20mafra&lowPriorityStreetsSearch=true&levels%5B0%5D=REGION&levels%5B1%5D=SUBREGION&levels%5B2%5D=CITY&levels%5B3%5D=DISTRICT&levels%5B4%5'
+    'https://www.imovirtual.com/comprar/apartamento/anadia/?search%5Bregion_id%5D=1&search%5Bsubregion_id%5D=3'
+    response = requests.get(url)
+    print(response)
+    if response.status_code == 200:
+        data = response.json()
+        with open('data.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
     print(topologia)
-get_link(['1','2','3'],4,45)
+
+get_link('lisabon', 'c',['1','2','3'],4,45)
