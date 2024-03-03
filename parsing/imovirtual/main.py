1a53550717e67c2d5dd4fbdf53fd9cab86bc81fb
Electro05 <mishalogv@ya.ru>
2024-03-03 23:13:51 +0300

    DAM

--- parsing/imovirtual/main.py
+++ parsing/imovirtual/main.py
@@ -1,10 +1,11 @@
+import asyncio
 import json
 import time
 
 from bs4 import BeautifulSoup
 import requests
 
-from config import cookies, headers
+from portugal.Portugal.bot.config import cookies, headers
 
 
 def pars_exact_flat(url: str) -> dict:
@@ -68,7 +69,7 @@ def pars(from_date, to_date, city, area, type_topologia, min_price, max_price):
 def get_link(topologia: list, price_from, price_to):
     ful_url = ''
     price = ''
-    sortirovka=''
+    sortirovka = ''
     if price_from != '-':
         price += f'search%5Bfilter_float_price%3Afrom%5D={str(price_from)}'
     if price_to != '-':
@@ -95,10 +96,58 @@ def get_link_area(city, area):
         except IndexError:
             pass
     return link, city_name
+
+
 'https://www.imovirtual.com/arrendar/?search%5Bfilter_enum_rooms_num%5D%5B2%5D=10&locations%5B0%5D%5Bregion_id%5D=11&locations%5B0%5D%5Bsubregion_id%5D=163&locations%5B1%5D%5Bregion_id%5D=11550728'
 'https://www.imovirtual.com/arrendar/?search%5Bfilter_enum_rooms_num%5D%5B0%5D=10&locations%5B0%5D%5Bregion_id%5D=11&locations%5B0%5D%5Bsubregion_id%5D=163&locations%5B1%5D%5Bregion_id%5D=11550728'
-if __name__ == "__main__":
-    pass
+
+
+def my_pars():
+    data = []
+    for i in range(1, 4):
+        URL = f'https://www.imovirtual.com/arrendar/?page={i}'
+        response = requests.get(URL, cookies=cookies, headers=headers)
+        html = BeautifulSoup(response.text, 'lxml')
+        list_with_all = list(map(lambda x: x.get('data-url'),
+                                 html.find('div', class_="row section-listing__row").find('div',
+                                                                                          class_="col-md-content section-listing__row-content").find_all(
+                                     'article')))
+        data += list_with_all
+    print(data)
+
+    return data
+
+
+def my_cont_pars(from_date, to_date, city, area, type_topologia, min_price, max_price):
+    data = set(my_pars())
+    # await asyncio.sleep(3600)
+    time.sleep(10800)
+    data_new = set(my_pars())
+    data = (data | data_new) - (data & data_new)
+    print(data)
+    for e in data:
+        response = requests.get(
+            e,
+            cookies=cookies,
+            headers=headers,
+        )
+        html = BeautifulSoup(response.text, 'lxml')
+        data = html.find_all('a', class_='css-1in5nid e19r3rnf1')
+        city_get = data[1].text
+        area_get = data[2].text
+        type_get = html.find_all('div', class_='css-1v52owc e1qm3vsd0')[1].find('div', "css-1ytkscc e1qm3vsd3").text[1]
+        price_get = html.find('header', class_='css-sos826 efcnut31').find('strong').text.split()
+        real_price = ''
+        for el in price_get:
+            if el.isdigit():
+                real_price += el
+
+        if city == city_get and max_price >= int(
+                real_price) >= min_price and type_get == type_topologia:
+            print(city_get, area_get, real_price, type_get,e, sep="|")
+
+
+my_cont_pars(1, 1, 'Porto', 'Vila Nova de Gaia', '2', 1000, 2000)
 # arr1 = pars()
 # arr1 = set(arr1)
 # time.sleep(1800)
